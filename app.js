9ca5373d2887a0b20f102680d9dba8947a836f7c
Jonathan Martens <jmartens8@yahoo.de>
2022-04-25 18:03:32 +0200

    Restaurant-CRUD-C done

--- app.js
+++ app.js
@@ -28,6 +28,9 @@ app.locals.appTitle = `${capitalized(projectName)} created with IronLauncher`;
 const index = require("./routes/index.routes");
 app.use("/", index);
 
+const restaurants = require("./routes/restaurants.routes");
+app.use("/", restaurants);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
